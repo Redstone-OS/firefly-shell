c84c3fe2b0ee7676a2c54ac611e34b478daf337e
João Victor P. <joaonhk@gmail.com>
2025-12-30 12:25:40 -0300

    ignore

--- target/x86_64-unknown-none/release/deps/shell-c99b4956381581ea.d
+++
@@ -1,6 +0,0 @@
-D:\Github\RedstoneOS\firefly\shell\target\x86_64-unknown-none\release\deps\shell-c99b4956381581ea.d: src\main.rs src\font.rs
-
-D:\Github\RedstoneOS\firefly\shell\target\x86_64-unknown-none\release\deps\shell-c99b4956381581ea: src\main.rs src\font.rs
-
-src\main.rs:
-src\font.rs:
