14c3042a71d2a60d974940b13c46879867b293e2
João Victor P. <joaonhk@gmail.com>
2025-12-30 07:34:45 -0300

    Shel

---
+++ target/x86_64-unknown-none/release/deps/shell-c99b4956381581ea.d
@@ -0,0 +1,6 @@
+D:\Github\RedstoneOS\firefly\shell\target\x86_64-unknown-none\release\deps\shell-c99b4956381581ea.d: src\main.rs src\font.rs
+
+D:\Github\RedstoneOS\firefly\shell\target\x86_64-unknown-none\release\deps\shell-c99b4956381581ea: src\main.rs src\font.rs
+
+src\main.rs:
+src\font.rs:
